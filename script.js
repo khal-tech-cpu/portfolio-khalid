b717f85322ffe86889db98f88e1894ef3768bc30
khal-tech-cpu <khalidboubacar6@gmail.com>
2026-01-03 21:43:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Message de bienvenue
+alert("Bienvenue sur le portfolio de Khalid 👋");
+
+// Changer le texte automatiquement
+document.addEventListener("DOMContentLoaded", function () {
+  const message = document.getElementById("message");
+  message.textContent = "Développeur web en apprentissage 🚀";
+});
+
+// Bouton interactif
+function direBonjour() {
+  alert("Merci d’avoir visité mon site 😄");
+}
